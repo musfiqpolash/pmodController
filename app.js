92e1c2f4d8891552ba06e6f7608c176316b2b87d
MD. MUSFIQUR RAHMAN <musfiqpolash@gmail.com>
2023-05-31 09:11:25 +0200

    bunch of changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const { Server } = require("socket.io");
 const { autoDetect } = require('@serialport/bindings-cpp');
 const { SerialPort, ReadlineParser } = require('serialport');
 
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -17,16 +16,18 @@ if (opsys == "win32" || opsys == "win64") {
 } else if (opsys == "linux") {
     device_path='/dev/ttyArduino';
 }
+
+// const Binding = autoDetect();
 // Binding.list().then(ports => {
-//     if (opsys == "win32" || opsys == "win64") {
-//         const port = ports.find(port => /2341/i.test(port.vendorId));
-//         if (port) device_path=port.path;
-//     } else if (opsys == "linux") {
-//         const port = ports.find(port => /8086/i.test(port.vendorId));
-//         if (port) device_path=port.path;
-//     }
-//     // const port = ports.find(port => /8086/i.test(port.vendorId));
-//     //console.log(device_path);
+    // if (opsys == "win32" || opsys == "win64") {
+    //     const port = ports.find(port => /2341/i.test(port.vendorId));
+    //     if (port) device_path=port.path;
+    // } else if (opsys == "linux") {
+    //     const port = ports.find(port => /8086/i.test(port.vendorId));
+    //     if (port) device_path=port.path;
+    // }
+    // const port = ports.find(port => /8086/i.test(port.vendorId));
+//     console.log(ports);
 // });
 
 const port = new SerialPort({
@@ -38,8 +39,11 @@ const port = new SerialPort({
     flowControl: false,
 });
 const parser = port.pipe(new ReadlineParser());
+
+app.use(express.static('public'));
+
 app.get("/", (req, res)=>{
-    res.sendFile('/public/index.html' , { root: '.' });
+    // res.sendFile('/public/index.html' , { root: '.' });
 });
 
 io.on('connection', (socket) => {
@@ -55,6 +59,9 @@ parser.on('data', function(data){
     io.emit('data',JSON.parse(data));
 });
 
+// setInterval(()=>{
+//     io.emit('webcam','some data');
+// }, 1000);
 
 server.listen(3000,()=>{
     console.log("listening on *.3000");
